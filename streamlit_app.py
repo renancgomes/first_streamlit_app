0d2a50dc1fe2ba636f802a2151f556490108a258
renancgomes <61101139+renancgomes@users.noreply.github.com>
2023-02-20 11:56:49 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ streamlit.title('The new healthy restaurant!')
 
 streamlit.header('Menu')
 
-streamlit.text('Food!')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Food!')
+streamlit.header('🍞 Breakfast Menu')
+streamlit.text('🥑 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
