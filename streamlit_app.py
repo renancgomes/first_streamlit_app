1c5428ccbf8e865b6525f3a0bb0c99f32b402526
renancgomes <61101139+renancgomes@users.noreply.github.com>
2023-02-27 16:57:27 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,9 @@ streamlit.dataframe(my_fruit_list)
 
 streamlit.header("Fruityvice Fruit Advice!")
 streamlit.text(fruityvice_response.json())
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.text(fruityvice_normalized)
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
