8482f1f649b023df68d484a85f8931bdf462c663
renancgomes <61101139+renancgomes@users.noreply.github.com>
2023-02-28 14:31:41 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
-import streamlit
-import pandas
-import requests
-import snowflake.connector
+#import streamlit
+#import pandas
+#import requests
+#import snowflake.connector
+from urllib.error import URLError
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 
@@ -40,6 +41,8 @@ streamlit.write('The user entered ', fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.stop
 
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
