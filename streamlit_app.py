1699785a70b95412dcc9daaae8ea10bcdfeb6b0c
renancgomes <61101139+renancgomes@users.noreply.github.com>
2023-02-28 14:18:39 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("The Fruit List Contains")
-streamlit.text(my_data_row)
+streamlit.header("The Fruit List Contains")
+streamlit.dataframe(my_data_row)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
